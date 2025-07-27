fffe7f27542a35c5b4466826b08732cb8ec3f93e
Jakub Mikołajek <jakub.mikolajek11@gmail.com>
2025-07-27 18:39:08 +0200

    removed unused module

--- apps/api/src/modules/embedding/embedding.module.ts
+++ apps/api/src/modules/embedding/embedding.module.ts
@@ -1,12 +1,10 @@
 import { HttpModule } from '@nestjs/axios'
 import { Module } from '@nestjs/common'
 
-import { EmbeddingController } from './embedding.controller'
 import { EmbeddingService } from './embedding.service'
 
 @Module({
   imports: [HttpModule],
-  controllers: [EmbeddingController],
   providers: [EmbeddingService],
   exports: [EmbeddingService],
 })
