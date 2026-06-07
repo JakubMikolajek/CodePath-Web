c411d29b3b9927fcc6011d56c82a7ff6da274f11
Jakub Mikołajek <jakub.mikolajek11@gmail.com>
2026-06-07 13:00:10 +0200

    added missing import

--- apps/api/src/modules/system-status/system-status.module.ts
+++ apps/api/src/modules/system-status/system-status.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 
+import { AuthModule } from '../auth/auth.module'
 import { DbModule } from '../db/db.module'
 import { QdrantModule } from '../qdrant/qdrant.module'
 import { RepoStorageModule } from '../repo-storage/repo-storage.module'
@@ -8,7 +9,7 @@ import { SystemStatusController } from './system-status.controller'
 
 @Module({
   controllers: [SystemStatusController],
-  imports: [DbModule, QdrantModule, RepoStorageModule],
+  imports: [AuthModule, DbModule, QdrantModule, RepoStorageModule],
   providers: [SystemStatusService]
 })
 export class SystemStatusModule { }
